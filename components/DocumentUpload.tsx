f2727838bc63a0503fcac574fb8349cd0dff3fc2
VRKotthapalli <vrk0803@gmail.com>
2025-11-27 11:50:25 -0500

    fix 2.1: return Buffer directly in NextResponse to resolve build type error

--- components/DocumentUpload.tsx
+++ components/DocumentUpload.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useState, useRef } from 'react'
-import { DocumentData } from '@/app/page'
+import { DocumentData } from '@/types'
 
 interface DocumentUploadProps {
   onUpload: (data: DocumentData) => void
